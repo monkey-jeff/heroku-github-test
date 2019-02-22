87b23668ace530a39503d67389509e1276b8017e
Jeff Albert <jalbert@salesforce.com>
2019-02-22 11:55:16 -0500

    Updated to use gradle instead of maven

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void index() {
+        Assert.assertTrue(true);
+    }
+}
